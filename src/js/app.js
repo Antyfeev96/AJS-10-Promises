d8c558fa1a98d2781561a0c7436ec7c26d5cd0b9
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-01-12 12:14:05 +0300

    Tests added

--- src/js/app.js
+++ src/js/app.js
@@ -5,34 +5,6 @@ import read from './Reader';
 
 export default class GameSavingLoader {
   load() {
-    return new Promise((resolve, reject) => {
-      const data = new Promise((resolve, reject) => {
-        resolve(read());
-      });
-      data.then((response) => console.log(json(response)));
-      resolve(data);
-    });
+    return read().then((response) => json(response));
   }
 }
-
-const loader = new GameSavingLoader();
-
-console.log(loader.load().then((response) => response));
-
-// export default class GameSavingLoader {
-//   load() {
-//     const data = new Promise((resolve, reject) => {
-//       resolve(console.log(read()));
-//     });
-//     const value = new Promise((resolve, reject) => {
-//       resolve(console.log(json(data)));
-//     });
-//     data.then(value);
-//     value.then((response) => console.log(response));
-//     return [data, value];
-//   }
-// }
-
-// const loader = new GameSavingLoader();
-
-// console.log(loader.load());
